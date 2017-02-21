c6173ab2dabc999e4935ac8596d5b990319addc1
Carlos Munoz Kampff and Dan Lauby <dan@>
2017-02-21 10:43:01 -0800

    Create Category class

--- src/Task.php
+++ src/Task.php
@@ -1,66 +1,75 @@
 <?php
-
-class Task
-{
-    private $description;
-    private $id;
-
-    function __construct($new_description, $new_id = null)
+    class Task
     {
-        $this->setDescription($new_description);
-        $this->id = $new_id;
-    }
+        private $description;
+        private $category_id;
+        private $id;
 
-    function setDescription($new_description)
-    {
-        $this->description = (string) $new_description;
-    }
+        function __construct($description, $id = null, $category_id)
+        {
+            $this->description = $description;
+            $this->id = $id;
+            $this->category_id = $category_id;
+        }
 
-    function getDescription()
-    {
-        return $this->description;
-    }
+        function setDescription($new_description)
+        {
+            $this->description = (string) $new_description;
+        }
 
-    function save()
-    {
-        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
-        $this->id = $GLOBALS['DB']->lastInsertId();
-    }
+        function getDescription()
+        {
+            return $this->description;
+        }
 
-    function getId()
-    {
-        return $this->id;
-    }
+        function getId()
+        {
+            return $this->id;
+        }
 
-    static function find($search_id)
-    {
-        $found_Task = null;
-        $tasks = Task::getAll();
-        foreach($tasks as $task) {
-            $task_id = $task->getId();
-            if ($task_id == $search_id) {
-                $found_Task = $task;
+        function getCategoryId()
+        {
+            return $this->category_id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id) VALUES ('{$this->getDescription()}', {$this->getCategoryId()})");
+            // NOTE {$this->getCategoryId()} does not have quotes because you are looking to return an integer !!!
+
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+            $tasks = array();
+            foreach($returned_tasks as $task) {
+                $description = $task['description'];
+                $id = $task['id'];
+                $category_id = $task['category_id'];
+                $new_task = new Task($description, $id, $category_id);
+                array_push($tasks, $new_task);
             }
+            return $tasks;
         }
-        return $found_Task;
-    }
 
-    static function getAll()
-    {
-        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
-        $tasks = [];
-        foreach($returned_tasks as $task) {
-            $description = $task['description'];
-            $id = $task['id'];
-            $new_task = new Task($description, $id);
-            array_push($tasks, $new_task);
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM tasks;");
         }
-        return $tasks;
-    }
 
-    static function deleteAll()
-    {
-        $GLOBALS['DB']->exec("DELETE FROM tasks");
+        static function find($search_id)
+        {
+            $found_task = null;
+            $tasks = Task::getAll();
+            foreach($tasks as $task) {
+                $task_id = $task->getId();
+                if ($task_id == $search_id) {
+                  $found_task = $task;
+                }
+            }
+            return $found_task;
+        }
     }
-}
 ?>
